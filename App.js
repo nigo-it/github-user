29b66261c0f2dc8cfff08110385e8a8b3df9ee2c
Nicola Sheldrick <nicola@geeny.io>
2017-11-21 21:14:35 +0100

    Add search button

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Finder from './src/containers/Finder';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello APP</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+        <Finder />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

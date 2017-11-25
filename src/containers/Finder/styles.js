4a94117bed5a3005dd5495aa75d541cede0a9e4d
rodrigo-morais <morais.rm@gmail.com>
2017-11-25 12:49:32 +0100

    Fixing style.

--- src/containers/Finder/styles.js
+++ src/containers/Finder/styles.js
@@ -3,42 +3,44 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create(
   {
     container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center'
     },
-
-      sometext: {
-          fontSize: 28
-      },
-
-      searchbox: {
-          marginTop: 6,
-          borderStyle: 'solid',
-          borderColor: '#000',
-          flexDirection: 'row',
-          alignSelf: 'stretch',
-          borderWidth: 1,
-          fontSize: 42
-      },
-      button: {
-          height: 45,
-          flexDirection: 'row',
-          backgroundColor: 'lightblue',
-          borderColor: 'lightblue',
-          borderWidth: 1,
-          borderRadius: 8,
-          marginBottom: 10,
-          marginTop: 10,
-          alignSelf: 'stretch',
-          justifyContent: 'center'
-      },
-      buttonText: {
-          fontSize: 18,
-          color: '#111',
-          alignSelf: 'center'
-      }
+    sometext: {
+      fontSize: 28
+    },
+    searchbox: {
+      marginTop: 6,
+      marginLeft: '5%',
+      borderStyle: 'solid',
+      borderColor: '#000',
+      flexDirection: 'row',
+      width: '90%',
+      alignSelf: 'stretch',
+      borderWidth: 1,
+      fontSize: 42
+    },
+    button: {
+      height: 45,
+      width: '90%',
+      flexDirection: 'row',
+      backgroundColor: 'lightblue',
+      borderColor: 'lightblue',
+      borderWidth: 1,
+      borderRadius: 8,
+      marginBottom: 10,
+      marginTop: 10,
+      marginLeft: '5%',
+      alignSelf: 'stretch',
+      justifyContent: 'center'
+    },
+    buttonText: {
+      fontSize: 18,
+      color: '#111',
+      alignSelf: 'center'
+    }
   }
 )
 
